4f54251021a9dfbc845ef4662e8fa60b4f519148
AChumber <avtarchumber@hotmail.com>
2023-05-30 20:46:28 +0100

    Move featured bg image into own div. Make nav translucent, so bg image show. Add border radius to nav bottom corners

--- src/components/pages/homePage/featuredMovie/FeaturedMovie.js
+++ src/components/pages/homePage/featuredMovie/FeaturedMovie.js
@@ -17,36 +17,39 @@ const FeaturedMovie = () => {
     }, [dispatch, trendingList.length])
 
     return (
-        <div className="featured-movie">
-            {
-                (!isLoading && popularMovie.hasOwnProperty('title')) ? (
-                <>
-                    <div className="featured-movie-bg-img" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${popularMovie.backdrop_path})`,
-                        backgroundPosition: 'center',
-                        backgroundSize: 'cover',
-                        backgroundRepeat: 'no-repeat',
-                        filter: 'blur(3px)' 
-                    }}></div>
-                    <div className="featured-movie-details">
-                        <div className='featured-movie-poster-container'>
-                            <img className="featured-movie-poster" src={'https://image.tmdb.org/t/p/w400'+popularMovie.poster_path} alt={`${popularMovie.title} poster`} />
+        (!isLoading && popularMovie.hasOwnProperty('title')) ? (
+            <>
+            <div className='featured-movie-bg-img-container'>
+                <img src={`https://image.tmdb.org/t/p/original${popularMovie.backdrop_path}`} alt='' />
+            </div>
+            <div className="featured-movie">
+            {/* <div className="featured-movie-bg-img" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${popularMovie.backdrop_path})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                filter: 'blur(0px)' 
+            }}></div> */}
+                <div className="featured-movie-details">
+                    <div className='featured-movie-poster-container'>
+                        <img className="featured-movie-poster" src={'https://image.tmdb.org/t/p/w400'+popularMovie.poster_path} alt={`${popularMovie.title} poster`} />
+                    </div>
+                    <div className="featured-movie-card">
+                        <div className="featured-movie-text">
+                            <h1><b>{ popularMovie.title }</b></h1>
+                            <p>{ popularMovie.overview }</p>
                         </div>
-                        <div className="featured-movie-card">
-                            <div className="featured-movie-text">
-                                <h1><b>{ popularMovie.title }</b></h1>
-                                <p>{ popularMovie.overview }</p>
-                            </div>
-                            <div className="featured-movie-btns">
-                                <Link to={`/movie/${popularMovie.id}`} className="featured-cta">Explore Movie</Link>
-                            </div>
+                        <div className="featured-movie-btns">
+                            <Link to={`/movie/${popularMovie.id}`} className="featured-cta">Explore Movie</Link>
                         </div>
                     </div>
-                </>
-                ) : (
-                    <SkeletonFeatureMovie />
-                )
-            }
-        </div>
+                </div>
+            </div>
+        </>
+        ) : (
+            <div className="featured-movie">
+                <SkeletonFeatureMovie />
+            </div>
+        )
     )
 }
 
